cc3e59eaf8d4add0365066e74cc96fb7a18457ce
hyabc <hyabc0@gmail.com>
2021-06-17 09:45:24 +0800

    Header file

---
+++ railway.h
@@ -0,0 +1,11 @@
+#include <gst/gst.h>
+#include <gst/player/player.h>
+#include "railwaylib.h"
+
+#ifndef __RAILWAY_H__
+#define __RAILWAY_H__
+
+void music_position_update_cb(GstPlayer*, GstClockTime, void*);
+void play_song(song_type*);
+
+#endif
